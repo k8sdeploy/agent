fb7715dffd56d02fc0c0c670330dc6c72b2d57fa
Keloran <keloran@develbox.info>
2022-09-05 21:04:57 +0100

    update service looking at correct secret

--- internal/config/k8sdeploy.go
+++ internal/config/k8sdeploy.go
@@ -5,8 +5,8 @@ import "github.com/caarlos0/env/v6"
 type K8sDeploy struct {
 	APIAddress    string `env:"API_ADDRESS" envDefault:"https://api.k8sdeploy.dev/v1"`
 	SocketAddress string `env:"SOCKET_ADDRESS" envDefault:"https://sockets.chewedfeed.com"`
-	Key           string `env:"K8SDEPLOY_KEY" envDefault:""`
-	Secret        string `env:"K8SDEPLOY_SECRET" envDefault:""`
+	Key           string `env:"K8SDEPLOY_API_KEY" envDefault:""`
+	Secret        string `env:"K8SDEPLOY_API_SECRET" envDefault:""`
 	CompanyID     string `env:"K8SDEPLOY_COMPANY_ID" envDefault:""`
 }
 
